fffc41332f90d9656c96fb128358c1864c07c214
Joseph Chambers <imcodingideas@Josephs-MacBook-Pro.local>
2017-03-18 22:34:25 -0700

    Update and Delete records with tests

--- test/controllers/drivers_controller_test.js
+++ test/controllers/drivers_controller_test.js
@@ -7,7 +7,7 @@ const
 const Driver = mongoose.model('driver');
 
 describe('Drivers controller', () => {
-    it('POST to /api/drivers creates a new driver', (done)=>{
+    it('POST to /api/drivers creates a new driver', done => {
         Driver.count().then(count => {
             request(app)
                 .post('/api/drivers')
@@ -19,5 +19,39 @@ describe('Drivers controller', () => {
                     });
                 });
         });
+    });
+
+    it('PUT to /api/drivers/:id edits an existing driver', done => {
+        const driver = new Driver({ email: 't@t.com', driving: false })
+
+        driver.save().then(() => {
+            request(app)
+                .put(`/api/drivers/${driver._id}`)
+                .send({ driving: true })
+                .end(() => {
+                    Driver.findOne({ email: 't@t.com' })
+                        .then(driver => {
+                            assert(driver.driving === true);
+                            done();
+                        });
+                });
+        });
+    });
+
+    it('DELETE to /api/drivers/:id can delete a driver', done => {
+        const driver = new Driver({ email: 'test@test.com' });
+
+        driver.save().then(() => {
+            request(app)
+                .delete(`/api/drivers/${driver._id}`)
+                .end(() => {
+                    Driver.findOne({ email: 'test@test.com' })
+                        .then(driver => {
+                            assert(driver === null);
+                            done();
+                        });
+                })
+        });
+
     });
 })
